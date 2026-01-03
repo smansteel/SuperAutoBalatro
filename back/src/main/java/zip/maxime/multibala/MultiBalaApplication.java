005346e3b6474d2476b43a10b769779001008dab
sman_steel <smansteelyt@gmail.com>
2026-01-03 10:54:24 +0100

    init2

---
+++ back/src/main/java/zip/maxime/multibala/MultiBalaApplication.java
@@ -0,0 +1,13 @@
+package zip.maxime.multibala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiBalaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiBalaApplication.class, args);
+    }
+
+}
---
+++ back/src/test/java/zip/maxime/multibala/MultiBalaApplicationTests.java
@@ -0,0 +1,13 @@
+package zip.maxime.multibala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiBalaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
